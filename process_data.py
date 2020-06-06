2a4aa87d8f1986412a731ea714725273bfdab7ae
Helen <helenbatson@gmail.com>
2020-06-06 23:01:41 +0100

    Add function to create booleans
    Add function to replace 2s with 1s in process_data.py

--- process_data.py
+++ process_data.py
@@ -5,16 +5,16 @@ import sqlite3
 import sqlalchemy as db
 from sqlalchemy import create_engine
 
-
-
-
 def load_data(messages_filepath, categories_filepath):
-    messages = pd.read_csv('messages.csv')
-    categories = pd.read_csv('categories.csv')
+    messages = pd.read_csv('disaster_messages.csv')
+    categories = pd.read_csv('disaster_categories.csv')
 
+    df = pd.merge(messages, categories, on='id')
+
+    return df
 
 def clean_data(df):
-    df = pd.merge(messages, categories, on='id')
+
     '''
     Split `categories` into separate category columns.
     - Split the values in the `categories` column on the `;`
@@ -45,61 +45,57 @@ def clean_data(df):
         category_colnames.append(newcol)
 
     categories.columns = category_colnames
-    #def create_booleans():
-    # for each column, find 0 / 1 in column values and replace
-    for column in categories.columns:
-        zero_one = []
 
-        # set each value to be the last character of the string
-        # convert column from string to numeric
-        for value in categories[column]:
-            zero_one.append(int(value[-1:]))       #[int(char) for char in b.split() if char.isdigit()])
 
-        categories[column]=zero_one
+    # for each column in the df, ensure 0 / 1 in column values; replace 2s with 1s
+    def create_booleans(df):
+        for column in df.columns:
+            zero_one = []
+
+            # set each value to be the last character of the string
+            # convert column from string to numeric
+            for value in df[column]:
+                zero_one.append(int(value[-1:]))
+
+            # Change value 2 to value 1 for boolean values throughout the matrix
+            zero_one = np.array(zero_one)
+            zero_one = np.where(zero_one==2, 1, zero_one)
+
+            df[column]=zero_one
+
+    create_booleans(categories)
 
     #Replace `categories` column in `df` with new category columns.
     #- Drop the categories column from the df dataframe since it is no longer needed.
     #- Concatenate df and categories data frames.
 
-    df_new = pd.merge(df.reset_index(drop=True),
+    df = pd.merge(df.reset_index(drop=True),
                  categories.reset_index(drop=True),
                  left_index=True,
                  right_index=True)
 
-    #Remove rows where boolean is 2
-    not_bool = df_new[df_new.related == 2]
-    list_not_bool = list(not_bool.index)
-    df_new = df_new.drop(list_not_bool)
 
-    #Remove duplicates.
-    # Check how many duplicates are in this dataset.
-    # Drop the duplicates.
-    # Confirm duplicates were removed.
+    #Remove duplicates
+    df=df.drop_duplicates()
 
-    # drop duplicates
-    df_new=df_new.drop_duplicates()
+    # drop the id column as it will not be needed in ML pipelines
+    df=df.drop(['id'], axis=1)
 
-    # drop the original categories column from `df`
-    # and the y_pred columns without predictions
-    df_new=df_new.drop(['categories','tools','hospitals','shops','aid_centers','missing_people','child_alone','offer'], axis=1)
-
-    return df_new
-
+    # return clean df
+    return df
 
 
 def save_data(df, database_filename):
     #Save the clean dataset into an sqlite database.
     # Create the connection
     # Set echo=True to see all of the output that comes from our database connection.
-    engine = create_engine('sqlite:///data/DisasterResponse.db', echo=True)
+    engine = create_engine('sqlite:///DisasterResponse.db', echo=True)
     sqlite_connection = engine.connect()
 
     sqlite_table = "disaster_table"
-    df_new.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
+    df.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
 
     sqlite_connection.close()
-
-
 
 def main():
     if len(sys.argv) == 4:
